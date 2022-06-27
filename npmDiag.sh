b1df4caa3e79d0fb630d41212feff8cfcee2013e
Alexander Schneider <aschneider.em+GitHub@gmail.com>
2022-06-27 12:03:11 -0700

    Updated countdown method

--- npmDiag.sh
+++ npmDiag.sh
@@ -85,13 +85,6 @@ containerLogCollect() {
         rm /var/lib/docker/containers/"${containerIDs[i]}"/"${containerIDs[i]}"-json.log
         docker start ${containerIDs[i]} > /dev/null 2>&1
 
-#        echo "Waiting for $(docker ps -a --format '{{.Names}}' -f "id=${containerIDs[i]}") to finish starting up..."
-#        waitTime=60
-#        completeTime="$(($(date +%s) + $waitTime))"
-#        while [ "$completeTime" -ge `date +%s` ]; do
-#            currentTime="$(( $completeTime - `date +%s` ))"
-#            printf '%s\r' "$(date -u -d "@$currentTime" +%M:%S)"
-#        done
         waitTime=60
         while [ $waitTime -gt 0 ]; do
            echo -ne "Seconds remaining: $waitTime\033[0K\r"
